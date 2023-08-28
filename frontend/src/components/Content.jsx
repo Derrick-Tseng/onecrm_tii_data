129960ab13901fb26bfeee127e4b243ecf43dc8d
terry32862 <terry66016@gmail.com>
2023-08-28 10:07:35 +0800

    modify the company name selection

--- frontend/src/components/Content.jsx
+++ frontend/src/components/Content.jsx
@@ -2,40 +2,7 @@ import '../css/Content.css';
 import Popup from './Popup';
 import React, {useState} from "react";
 import { apiGetInfoById, apiGetInfoList, apiGetInfoListSearchbox } from "../api/agent.js";
-<div class="container">
-  <ul class="responsive-table">
-    <li class="table-header">
-      <div class="col col-1">Job Id</div>
-      <div class="col col-2">Customer Name</div>
-      <div class="col col-3">Amount Due</div>
-      <div class="col col-4">Payment Status</div>
-    </li>
-    <li class="table-row">
-      <div class="col col-1" data-label="Job Id">42235</div>
-      <div class="col col-2" data-label="Customer Name">John Doe</div>
-      <div class="col col-3" data-label="Amount">$350</div>
-      <div class="col col-4" data-label="Payment Status">Pending</div>
-    </li>
-    <li class="table-row">
-      <div class="col col-1" data-label="Job Id">42442</div>
-      <div class="col col-2" data-label="Customer Name">Jennifer Smith</div>
-      <div class="col col-3" data-label="Amount">$220</div>
-      <div class="col col-4" data-label="Payment Status">Pending</div>
-    </li>
-    <li class="table-row">
-      <div class="col col-1" data-label="Job Id">42257</div>
-      <div class="col col-2" data-label="Customer Name">John Smith</div>
-      <div class="col col-3" data-label="Amount">$341</div>
-      <div class="col col-4" data-label="Payment Status">Pending</div>
-    </li>
-    <li class="table-row">
-      <div class="col col-1" data-label="Job Id">42311</div>
-      <div class="col col-2" data-label="Customer Name">John Carpenter</div>
-      <div class="col col-3" data-label="Amount">$115</div>
-      <div class="col col-4" data-label="Payment Status">Pending</div>
-    </li>
-  </ul>
-</div>
+
 
 function setModal(productNum, setModalContentId, setModalContent){
     setModalContentId(productNum);
@@ -43,6 +10,10 @@ function setModal(productNum, setModalContentId, setModalContent){
     .then(res =>{
         const item = res.data[0];
         setModalContent(
+
+
+
+                
             <>
                <table className="styled-table">
             <tbody>
