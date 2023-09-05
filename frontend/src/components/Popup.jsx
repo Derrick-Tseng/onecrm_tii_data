b82bd93d3cb1866ee35e257e8dbcaa1b9dac0c96
terry32862 <terry66016@gmail.com>
2023-09-05 16:17:29 +0800

    popup title fix

--- frontend/src/components/Popup.jsx
+++ frontend/src/components/Popup.jsx
@@ -16,10 +16,10 @@ function Popup({productNum, content, setModalContent}) {
                         <div className="modal-header">
                             <h5 className="modal-title text-danger" id="exampleModalLabel">詳細資料{productNum}</h5>
                             <div className="button-group">
-                            <button type="button" onClick={() => getProductInfo(productNum, setModalContent)}>基本資料</button>
-                            <button type="button" onClick={() => getProductContentPdf(productNum, setModalContent)}>產品簡章</button>
-                            <button type="button" onClick={() => getTreatyPdf(productNum, setModalContent)}>條款</button>
-                            <button type="button" onClick={() => getRatePdf(productNum, setModalContent)}>費率</button>
+                            <button className="button" type="button" onClick={() => getProductInfo(productNum, setModalContent)}>基本資料</button>
+                            <button className="button" type="button" onClick={() => getProductContentPdf(productNum, setModalContent)}>產品簡章</button>
+                            <button className="button" type="button" onClick={() => getTreatyPdf(productNum, setModalContent)}>條款</button>
+                            <button className="button" type="button" onClick={() => getRatePdf(productNum, setModalContent)}>費率</button>
                         </div>
                             <button type="button" onClick={()=>setModalContent(null)} className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
