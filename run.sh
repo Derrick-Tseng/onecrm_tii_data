cf43fdaa9bf44916f2a64ffed38cc91a572fca82
Derrick-Tseng <72126473+Derrick-Tseng@users.noreply.github.com>
2023-09-05 13:59:27 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,10 +5,10 @@ cd frontend
 sudo docker build -t frontend-test .
 cd ..
 sudo docker compose up
-# echo Start Seeding Test Data
-# sleep 2
-# cd testData
-# pip install psycopg2
-# python3 dataSeeding.py
-# echo Data Seeding Finished
-# cd ..
+echo Start Seeding Test Data
+sleep 2
+cd testData
+pip install psycopg2
+python3 dataSeeding.py
+echo Data Seeding Finished
+cd ..
