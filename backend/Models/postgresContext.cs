f3c04157bb70239d919d249f5a5b6b7bfadb598e
Derrick-Tseng <derricktseng@Derricks-MacBook-Pro.local>
2023-09-05 16:29:50 +0800

    fix connStr in postgresContext.cs

--- backend/Models/postgresContext.cs
+++ backend/Models/postgresContext.cs
@@ -23,8 +23,9 @@ namespace backend.Models
         {
             if (optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseNpgsql("Server=127.0.0.1;Port=5432;Database=postgres;User Id=root;Password=postgres;");
+                var builder = WebApplication.CreateBuilder();
+                // optionsBuilder.UseNpgsql("Server=127.0.0.1;Port=5432;Database=postgres;User Id=root;Password=postgres;");
+                optionsBuilder.UseNpgsql(builder.Configuration.GetConnectionString("postgres"));
             }
         }
 
