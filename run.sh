9d85d3cb6bbad1e7bc780536f5abe7e3751f0db0
Derrick-Tseng <derricktseng@2300530Derrick.local>
2023-09-13 11:32:58 +0800

    fix run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 sudo docker compose up -d
-echo Start Seeding Test Data
-sleep 2
-cd testData
-pip install psycopg2
-python3 dataSeeding.py
-echo Data Seeding Finished
-cd ..
+# echo Start Seeding Test Data
+# sleep 2
+# cd testData
+# pip install psycopg2
+# python3 dataSeeding.py
+# echo Data Seeding Finished
+# cd ..
